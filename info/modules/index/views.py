560932fb7be538a87d7208e105f8c3988fb10de1
wanglong-github <59468022+wanglong-github@users.noreply.github.com>
2020-06-16 21:19:37 +0800

    ico

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,7 +1,12 @@
-
 # 蓝图
 from . import index_blu
-from flask import render_template
+from flask import render_template, current_app
+
+
 @index_blu.route('/')
 def index():
     return render_template('news/index.html')
+
+@index_blu.route('/favicon.ico')
+def get_web_logo():
+    return current_app.send_static_file('news/favicon.ico')
