79f4336584a3833bddf37117db66ac3df21091f8
wanglong-github <59468022+wanglong-github@users.noreply.github.com>
2020-06-16 17:44:19 +0800

    项目拆分

---
+++ info/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask, session
+from flask_session import Session
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import CSRFProtect
+
+from config import Config
+app = Flask(__name__)
+
+def create_app(config):
+    pass
+
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+# 初始化redis配置
+# redis.StrictRedis(host=Config.RDIES_HOST, port=Config.RDIES_PORT)
+
+# 开启csrf 保护， 只用于服务器验证功能
+CSRFProtect(app)
+# 设置session保存指定位置
+Session(app)
