26af383f40f5c5181794d28e6eadce3f97d8ecc4
wanglong-github <59468022+wanglong-github@users.noreply.github.com>
2020-06-23 14:20:12 +0800

    分类

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, current_app, session
 
 from info import constants
-from info.models import User, News
+from info.models import User, News, Category
 from . import index_blu
 
 
@@ -27,10 +27,16 @@ def index():
     click_news_list = []
     for news in news_list if news_list else []:
         click_news_list.append(news.to_basic_dict())
+    categories_dicts = []
+    "获取新闻分分类数据"
+    categories = Category.query.all()
+    for category in categories:
+        categories_dicts.append(category.to_dict())
     data = {
         "user_info": user.to_dict() if user else None,
         "click_news_list": click_news_list,
-    }
+        "categories": categories_dicts
+   }
 
     return render_template('news/index.html', data=data)
 
